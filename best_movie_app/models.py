54dfa2a4f875b78442ff3625c2bfa1417dd5fd78
Vladislav <vdronovvladislav@gmail.com>
2024-04-26 13:29:10 +0300

    Доработал проект. Убрал уникальность поле name в модели Movie + добавил возможность пользоваться ссылками на картинки

--- best_movie_app/models.py
+++ best_movie_app/models.py
@@ -5,8 +5,10 @@ from . import db, app
 
 class Movie(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(128), nullable=False, unique=True)
+    name = db.Column(db.String(128), nullable=False)
     image = db.Column(db.String(255))
 
     def get_image_path(self):
+        if self.image.startswith('https'):
+            return self.image
         return os.path.join(app.config['UPLOAD_FOLDER'], self.image)
--- best_movie_app/views.py
+++ best_movie_app/views.py
@@ -18,59 +18,50 @@ def index_view():
 @app.route('/start_tournament', methods=['POST'])
 def start_tournament():
     """Вью-функция страницы соревнования."""
-    if request.method == 'POST':
-        try:
-            tournament_list = session['tournament_list']
-            winners_list = session['winners_list']
-
-            chosen_film_id = int(request.form['chosen_film'])
-            winners_list.append(chosen_film_id)
-            session['winners_list'] = winners_list
-
-            if not tournament_list:
-                if len(winners_list) == 1:
-                    winner = Movie.query.get(winners_list[0])
-                    return render_template('winner.html', winner=winner)
-                else:
-                    tournament_list.extend(winners_list)
-                    winners_list.clear()
-                    session['winners_list'] = winners_list
-                    session['tournament_list'] = tournament_list
-
-            left_film_id = random.choice(tournament_list)
-            tournament_list.remove(left_film_id)
-            right_film_id = random.choice(tournament_list)
-            tournament_list.remove(right_film_id)
-            session['tournament_list'] = tournament_list
-
-            left_film = Movie.query.get(left_film_id)
-            right_film = Movie.query.get(right_film_id)
-            return render_template(
-                    'choice.html', left_film=left_film, right_film=right_film
-                )
-
-        except KeyError:
-            film_count = int(request.form['filmCount'])
-            session['tournament_list'] = [
-                movie.id for movie in Movie.query.limit(film_count).all()
-            ]
-            session['winners_list'] = []
-            session['temporary_list'] = []
-
-            tournament_list = session['tournament_list']
-            winners_list = session['winners_list']
-
-            left_film_id = random.choice(tournament_list)
-            tournament_list.remove(left_film_id)
-            right_film_id = random.choice(tournament_list)
-            tournament_list.remove(right_film_id)
-            session['tournament_list'] = tournament_list
-
-            left_film = Movie.query.get(left_film_id)
-            right_film = Movie.query.get(right_film_id)
-            return render_template(
+    try:
+        tournament_list = session['tournament_list']
+        winners_list = session['winners_list']
+        chosen_film_id = int(request.form['chosen_film'])
+        winners_list.append(chosen_film_id)
+        session['winners_list'] = winners_list
+        if not tournament_list:
+            if len(winners_list) == 1:
+                winner = Movie.query.get(winners_list[0])
+                return render_template('winner.html', winner=winner)
+            else:
+                tournament_list.extend(winners_list)
+                winners_list.clear()
+                session['winners_list'] = winners_list
+                session['tournament_list'] = tournament_list
+        left_film_id = random.choice(tournament_list)
+        tournament_list.remove(left_film_id)
+        right_film_id = random.choice(tournament_list)
+        tournament_list.remove(right_film_id)
+        session['tournament_list'] = tournament_list
+        left_film = Movie.query.get(left_film_id)
+        right_film = Movie.query.get(right_film_id)
+        return render_template(
                 'choice.html', left_film=left_film, right_film=right_film
             )
+    except KeyError:
+        film_count = int(request.form['filmCount'])
+        session['tournament_list'] = [
+            movie.id for movie in Movie.query.limit(film_count).all()
+        ]
+        session['winners_list'] = []
+        session['temporary_list'] = []
+        tournament_list = session['tournament_list']
+        winners_list = session['winners_list']
+        left_film_id = random.choice(tournament_list)
+        tournament_list.remove(left_film_id)
+        right_film_id = random.choice(tournament_list)
+        tournament_list.remove(right_film_id)
+        session['tournament_list'] = tournament_list
+        left_film = Movie.query.get(left_film_id)
+        right_film = Movie.query.get(right_film_id)
+        return render_template(
+            'choice.html', left_film=left_film, right_film=right_film
+        )
 
 
 @app.route('/add', methods=['POST', 'GET'])
@@ -81,7 +72,8 @@ def add_movie_view():
         image = form.image.data
         name = form.name.data
         if Movie.query.filter_by(name=name).first():
-            flash('Этот фильм уже добавлен!')
+            flash('Этот фильм уже добавлен! '
+                  'Попробуйте ввести оригинальное название.')
             return render_template('add_movie.html', form=form)
         if not image:
             flash('У фильма должен быть постер!')
